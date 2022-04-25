5764123dde1c013d76d8a43112590b531f69e0b8
ChaicWong <chai.cwong@yahoo.com>
2022-04-24 23:52:33 -0700

    commit on 'master'

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ searchBtn.addEventListener('click', getMealList);
 
 
 //get meal list that matches with the ingridents
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`).then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals) {
+            data.meals.forEach(meal => {
+                html += `
+                        <div class="meal-item" data-id = "${meal.idMeal}>
+                            <div class="meal-img">
+                                <img src= "${meal.strMealThumb}" alt="food">
+                            </div>
+                            <div class="meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a href="#" class="recipe-btn">Get Recipe</a>
+                            </div>
+                        </div>`;
+            });
+        }
+
+        mealList.innerHTML = html;
+    });
+}
